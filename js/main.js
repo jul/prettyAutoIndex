59cfa5219f8619923d0ff762e0d6a43ba264f8a6
Julien Tayon <percol@ti0n.net>
2010-09-23 16:31:58 +0200

    I f**** hate presentation stuff

--- js/main.js
+++ js/main.js
@@ -16,8 +16,7 @@ $(document).ready(
                     function() { 
                         $('#switcher').themeswitcher( { loadTheme : "Cuppertino" } ); 
                         $('#switcher > a').addClass("noimage");
-                        $('body,pre ').addClass('ui-widget-content');
-                        $('body,pre ').css('border', '0px');
+                        $('pre ').addClass('ui-widget-content');
                         $.getScript("/org/js/jquery-ui-1.8.5.custom.min.js", 
                             function () { 
                                 $.getScript("/org/js/jul.js");
