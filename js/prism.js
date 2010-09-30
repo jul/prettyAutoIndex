335085d91101b500091ee8cf652f22c8373ea090
Julien Tayon <percol@ti0n.net>
2010-09-30 17:10:35 +0200

    ajout des modifs de jeremy (autostart de la gallerie)

--- js/prism.js
+++ js/prism.js
@@ -1,4 +1,4 @@
-        /* 
+/* 
 To open an image gallery simply:
 
    1. Create a link (<a href=”#”>).
@@ -7,13 +7,31 @@ To open an image gallery simply:
    4. Pass the following settings when you initialize prettyPhoto: $(“a[rel^='prettyPhoto']“).prettyPhoto({theme: ‘facebook’,slideshow:5000, autoplay_slideshow:true});
 
 */
+        
         $("a").not(".noimage").attr("rel","prettyPhoto[gallery]");
-        $("a").not(".noimage").each( function() { 
+        $("a").not(".noimage").prettyPhoto({
+        	animation_speed:		"normal",			
+        	overlay_gallery:		false,
+        	opacity: 		0.5
+        });
+        
+        var images = new Array;
+        $("a").not(".noimage").each(function(){
+        	images.push($(this).attr("href"));
+        });
+        //console.log(images);
+        $.prettyPhoto.open(images);
+        
+        /*
+				$("a").not(".noimage").each( function() { 
             $(this).attr("alt", $(this).attr("href"));
             });
 
         $("a").not(".noimage").attr("href", "#");
-        $("a").not(".noimage").prettyPhoto();
+        
+        
+        
         $("a").not(".noimage").each( function() { 
             $(this).attr("href", $(this).attr("alt"));
             });
+				*/
